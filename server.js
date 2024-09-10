5fa32aa61c26bd9b4091208c0cbe1a724c92c466
Emmanuel <kombae@gmatc.matc.edu>
2024-09-10 14:22:00 -0500

    add API endpoint

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ const app = express();
 
 connectDatabase();
 
+app.us(express.json({ extended: false }));
+
 //API endpoints
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
+
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+});
 
 //connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));
