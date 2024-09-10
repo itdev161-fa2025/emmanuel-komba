3bed2204ead9678e4441be820ee39c2e27d727b5
Emmanuel <kombae@gmatc.matc.edu>
2024-09-10 14:53:21 -0500

    add data validation to register user api endpoint

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDatabase from './config/db';
+import { check, validationResult } from 'express-validator';
 
 //Initialize express application
 const app = express();
@@ -13,7 +14,14 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
-app.post('/api/users', (req, res) => {
+app.post(
+    '/api/users', 
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'please enter a password with 6 or more characters').length({ min: 6 })
+    ],
+    (req, res) => {
     console.log(req.body);
     res.send(req.body);
 });
