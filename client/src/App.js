07ad47fe1d4396f65858f3ab0d71edd174cd59f3
Emmanuel Komba <kombae@gmatc.matc.edu>
2024-10-23 10:42:01 -0500

    Normalize line endings

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+      .then((response) => {
+        this.setState({
+          data: response.data
+        })
+      }).catch((error) => {
+        console.error(`Error fetching data: ${error}`);
+      })
+  }
+  
+  
+    render() {
+      return (
+        <div className='App'>
+        <header className='App-header'>
+          GoodThings
+        </header>
+      </div>
+    )
+}
 }
 
 export default App;
--- server.js
+++ server.js
@@ -2,15 +2,15 @@ import express from 'express';
 import connectDatabase from './config/db';
 import { check, validationResult } from 'express-validator';
 import cors from 'cors';
-import bcrypt from 'bcryptjs';
-import User from './models/User';
 
 //Initialize express application
 const app = express();
 
 connectDatabase();
 
+//Configure Middleware
 app.us(express.json({ extended: false }));
+app.use(cors({ origin: 'http://localhost:3000'}));
 
 //API endpoints
 app.get('/', (req, res) =>
@@ -24,36 +24,15 @@ app.post(
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'please enter a password with 6 or more characters').length({ min: 6 })
     ],
-    async (req, res) => {
+    (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(422).json({ errors: errors.array() })
     } else {
-        const { name, email, password } = req.body;
-        try {
-            let user = await User.findOne({ email: email });
-            if (user) {
-                return res
-                  .status(400)
-                  .json({ errors: [{ msg: 'User already exists' }] });
-            }
-
-            user = new User({
-                name: name,
-                email: email,
-                password: password
-            });
-
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(password, salt);
-
-            await user.save();
-            res.send('User successfully registered');
-        }   catch (error) {
-            res.status(500).send('Server error');
-        }
+        return res.send(req.body);
     }
 });
 
 //connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+const port = 5000;
+app.listen(port, () => console.log(`Express server running on port ${port}`));
