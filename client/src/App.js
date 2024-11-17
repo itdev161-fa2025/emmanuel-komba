6d3091e1f82b18f3b2b6c58ee80380c3c3936056
Emmanuel Komba <kombae@gmatc.matc.edu>
2024-11-17 17:07:43 -0600

    Update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,13 @@ import { Routes, Route, Link} from 'react-router-dom';
 import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
+import { response } from 'express';
 
 class App extends React.Component {
   state = {
-    data: null
+    data: null,
+    token: null,
+    user: null
   }
 
   componentDidMount() {
@@ -17,13 +20,53 @@ class App extends React.Component {
         this.setState({
           data: response.data
         })
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.error(`Error fetching data: ${error}`);
       })
+
+      this.authenticateUser();
   }
   
-  
+  authenticateUser = () => {
+    const token = localStorage.getItem('token');
+
+    if (!token) {
+      localStorage.removeItem('user');
+      this.setState({ user: null});
+    }
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      }
+
+      axios.get('http://localhost:500/api/auth', config)
+        .then((response) => {
+          localStorage.setItem('user', response.data.name)
+          this.setState({ user: response.data.name })
+        })
+        .catch((error) => {
+          localStorage.removeItem('user');
+          this.setState({ user:null });
+          console.error(`Error logging in: ${error}`)
+        })
+    }
+  }
+
+  logOut = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({ user: null, token: null })
+  }
+
     render() {
+      let { user, data } = this.state;
+      const authProps = {
+        authenticateUser: this.authenticateUser
+      }
       return (
         <div className='App'>
         <header className='App-header'>
@@ -31,14 +74,17 @@ class App extends React.Component {
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/register">Register</Link></li>
-            <li><Link to="/login">Login</Link></li>
+            {user ?
+              <li><Link to="" onClick={this.logOut}>Log out</Link></li> :
+              <li><Link to="/login">Log in</Link> </li>
+            }
           </ul>
         </header>
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/register" element={<Register />}/>
-            <Route path="/login" element={<Login />}/>
+            <Route path="/" element={<Home user={user} data={data}/>} />
+            <Route path="/register" element={<Register {...authProps}/>}/>
+            <Route path="/login" element={<Login {...authProps}/>}/>
           </Routes>
         </main>
 
